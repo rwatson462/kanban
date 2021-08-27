b3f5d47c423dfe32aa87b74a0e1c909359df6f8f
Rob Watson <rob.watson@therange.co.uk>
2021-08-27 19:18:54 +0100

    Update database setup script

--- src/setup/create_database.php
+++ src/setup/create_database.php
@@ -16,7 +16,7 @@ echo "Creating database\n";
 // to allow us to start from scratch
 if( $argv[1] ?? '' === '--drop' )
 {
-   $db->query('drop database kanban');
+   $db->query('drop database if exists kanban');
 }
 
 $db->query('create database if not exists kanban');
@@ -37,13 +37,67 @@ $db->query($query);
 
 echo "Creating other data tables\n";
 
+echo ">> Creating table cards\n";
 $query = <<<EOL
 
 create table if not exists cards (
    id int not null auto_increment,
    title varchar(255) not null,
    content text default null,
+   step_id int,
+   category_id int,
    primary key (id)
+)
+
+EOL;
+$db->query($query);
+
+echo ">> Creating table steps\n";
+$query = <<<EOL
+
+create table if not exists steps (
+   id int not null auto_increment,
+   title varchar(255),
+   description varchar(255),
+   primary key(id)
+)
+
+EOL;
+$db->query($query);
+
+echo ">> Creating table step_history\n";
+$query = <<<EOL
+
+create table if not exists step_history (
+   step_id int not null,
+   card_id int not null,
+   date_created datetime not null default NOW(),
+   key(step_id)
+)
+
+EOL;
+$db->query($query);
+echo ">> Creating table categories\n";
+$query = <<<EOL
+
+create table if not exists categories (
+   id int not null auto_increment,
+   title varchar(50),
+   icon varchar(20),
+   primary key(id)
+)
+
+EOL;
+$db->query($query);
+
+echo ">> Creating table category_history\n";
+$query = <<<EOL
+
+create table if not exists category_history (
+   category_id int not null,
+   card_id int not null,
+   date_created datetime not null default NOW(),
+   key(category_id)
 )
 
 EOL;
