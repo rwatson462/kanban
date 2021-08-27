2e91b5dcead84e8b917e218409aa6fc5f52614e4
Rob Watson <rob.watson@therange.co.uk>
2021-08-27 19:16:53 +0100

    Throw exception if template not found

--- src/function/template.php
+++ src/function/template.php
@@ -5,5 +5,7 @@ require dirname(__DIR__).'/config/template_config.php';
 function template( string $tpl_file, array $data = [] ): void
 {
    extract($data);
-   include TemplateRoot . '/' . $tpl_file;
+   $tpl_filename = TemplateRoot . '/' . $tpl_file . '.tpl';
+   if( file_exists($tpl_filename)) include $tpl_filename;
+   else throw new Exception("Template not found: $tpl_file");
 }
