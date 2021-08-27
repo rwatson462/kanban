33b1fa4dc94c0f20e5e00d08420da28e135808f8
Rob Watson <rwatson462@gmail.com>
2021-08-27 20:46:00 +0100

    Create add category route

---
+++ src/route/category/new.php
@@ -0,0 +1,28 @@
+<?php
+
+// this route is only usable on the POST method
+if($_SERVER['REQUEST_METHOD'] !== 'POST') {
+   header('Location: /404');
+   exit;
+}
+
+
+
+
+$category_name = $_POST['category_name'] ?? '';
+
+if(!$category_name) {
+   header('Content-type: text/plain');
+   echo "No category name given";
+   exit;
+}
+
+
+require_function('db_connect');
+$db = db_connect();
+
+$db->run('insert into categories set title = :title', [
+   'title' => $category_name
+]);
+
+header('Location: /settings');
