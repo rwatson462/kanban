1f2a08b3e444a7a8c25491c6925a628ffd13a7e9
Rob Watson <rob.watson@therange.co.uk>
2021-08-27 12:08:15 +0100

    Create welcome page

--- src/html/index.php
+++ src/html/index.php
@@ -1,3 +1,9 @@
 <?php
 
 include dirname(__DIR__).'/config.php';
+
+template('html_head.tpl', [
+   'styles' => ['welcome.css']
+]);
+template('welcome.tpl');
+template('html_foot.tpl');
---
+++ src/template/welcome.tpl
@@ -0,0 +1,6 @@
+<body class="welcome">
+   <article>
+      <h1>Kanban</h1>
+      <p class="title">From "to-do" to "to-done" in an organised fashion</p>
+   </article>
+</body>
