b2bc67ac2c7ee81b671c05fc82da314d18108a5b
Rob Watson <rob.watson@therange.co.uk>
2021-08-27 14:33:18 +0100

    Create single point of entry script

--- src/html/index.php
+++ src/html/index.php
@@ -1,9 +1,23 @@
 <?php
 
-include dirname(__DIR__).'/config.php';
+include dirname(__DIR__).'/config/route_config.php';
 
-template('html_head.tpl', [
-   'styles' => ['welcome.css']
-]);
-template('welcome.tpl');
-template('html_foot.tpl');
+// single point of entry for all requests
+// determine requested URI, check if file
+// exists, include it if it does
+$uri = $_SERVER['REQUEST_URI'];
+
+// special case '/'
+if( $uri === '/' )
+{
+   $uri = '/welcome';
+}
+
+if(!file_exists(RouteDirectory.$uri.'.php'))
+{
+   http_response_code(404);
+   include RouteDirectory.'/404.html';
+   exit;
+}
+
+include RouteDirectory.$uri.'.php';
